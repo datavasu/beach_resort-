17ed03a2a088841ddccbf5d2d04123fbcd90bf7b
Vasu Beri <vasu786beri@gmail.com>
2023-08-03 14:14:19 +0530

    c1

---
+++ src/Componemts/Header/index.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='bg-red-600 p-2'>
+        <div className='flex lg:space-x-44 space-x-36'>
+            <img className='w-20 h-12 rounded-full' src="https://d1csarkz8obe9u.cloudfront.net/posterpreviews/beach-resort-logo-design-template-d430830afb72efa8b21f3b44520f74b7_screen.jpg?ts=1614935495"></img>
+        {/* <div>logo</div> */}
+        <div className='flex space-x-20 font-bold text-white text-[20px]'>
+    <div className=''>Home</div>
+    <div>Rooms</div>
+   
+</div>
+        </div>
+   
+
+
+    </div>
+  )
+}
+
+export default Header
